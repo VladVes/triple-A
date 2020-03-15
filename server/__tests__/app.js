e05d38778469c6e13fd5bbec5bfd52494dad712b
VladVes <veselov.workflow@gmail.com>
2020-03-15 22:04:46 +0700

    add initial files and deps

---
+++ server/__tests__/app.js
@@ -0,0 +1,9 @@
+// import { app } from '../src/app.js';
+const app = require('../src/app.js');
+
+describe('app', () => {
+  test('return 0', () => {
+    const result = app.run();
+    expect(result).toBe(0);
+  });
+});
---
+++ server/src/app.js
@@ -0,0 +1,6 @@
+function run() {
+  console.log('Application has started.');
+  return 0;
+}
+
+exports.app = app;
